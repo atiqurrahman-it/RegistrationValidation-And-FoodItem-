773b177f855a70abc46f9ca93a38e1e15e4dac62
atiqur <atiqurrahman.cse2552@gmail.com>
2023-02-25 04:38:44 +0600

    all food items show & single food item details show

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 const foodSearch=document.getElementById('foodSearchBtn')
-const FoodInputElement=document.getElementById('FoodInputId')
 
-foodName=FoodInputElement.value
-FoodInputElement.value=''
 
-const FoodShow=(name)=>{
-    foodUrl=`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}
+const LoadFood=(searchText)=>{
+    foodUrl=`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}
     `
     fetch(foodUrl)
     .then(res=>res.json())
@@ -16,8 +13,11 @@ const FoodShow=(name)=>{
 
 const foodDisplay=meals=>{
     const cardContainer=document.getElementById('card-container')
+    // reset Food item when Search btn click than new item show 
+    // remove item and new item show 
+    cardContainer.innerText=''
     meals.forEach(meal=>{
-        console.log(meal)
+        // console.log(meal)
         const div=document.createElement('div');
         div.classList.add('col');
         div.innerHTML=`<div class="card mb-3" style="max-width: 540px">
@@ -33,7 +33,7 @@ const foodDisplay=meals=>{
               </p>
               <p class="card-text">
               </p>
-                <a class="text-warning text-decoration-underline" href='https://www.figma.com/file/sMQOrSzcBjp9Q7dbUPndF4/mealdb?node-id=1%3A7&t=lu7q0S7OFAct9y4X-0'>View Details</a>
+                <a onclick="FoodDetails(${meal.idMeal})" class="text-warning text-decoration-underline" data-bs-toggle="modal" data-bs-target="#FoodDatails">View Details</a>
               </p>
             </div>
           </div>
@@ -44,7 +44,59 @@ const foodDisplay=meals=>{
     })
 }
 
-FoodShow('fish')
+const FoodDetails=(itemId)=>{
+  url=`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${itemId}`
+
+  // fetch user korte partam but asyn & wait user kore krechi 
+  fetch(url)
+  .then(res=>res.json())
+  // single Food data
+  .then(data=>singleFoodDetails(data.meals[0]))
+  // if error 
+  .catch(error=>console.log(error))
+ 
+}
+
+const singleFoodDetails=data=>{
+  console.log(data)
+  const FoodTitle=document.getElementById('exampleModalLabel')
+  FoodTitle.innerText=data.strMeal
+  const modalBody=document.getElementById('modal-body')
+  const div=document.createElement('div')
+  modalBody.innerText=''
+  div.innerHTML=`
+  <img src="${data.strMealThumb}" class="img-fluid rounded-start w-100" style="max-height:250px;" alt="..." />
+  
+  <h4 class="my-2">Category : ${data.strCategory} </h4>
+  <h4 class="mb-2">Area : ${data.strArea} </h4>
+  <p class="text-muted"> ${data.strInstructions}</p>
+  <a href='${data.strYoutube}' target="_blank" >YouTube : ${data.strYoutube}</a>
+  `
+  modalBody.appendChild(div)
+  
+  console.log(FoodTitle)
+  console.log("single Food")
+}
+const SearchMeals=()=>{
+  const FoodInputElement=document.getElementById('FoodInputId')
+  foodName=FoodInputElement.value
+  FoodInputElement.value=''
+
+  const error=document.getElementById('errorMessage')
+  if(foodName.length==0){
+    error.innerText='Please Enter Your Food Name'
+    error.classList.add('bg-warning')
+  }else{
+    error.innerText=''
+    error.classList.remove('bg-warning')
+    // load function call 
+    LoadFood(foodName)
+  }
+ 
+}
+// when page load than call 
+LoadFood('fish')
 
 // window.addEventListener('load',FoodShow('fish'))
-// foodSearch.addEventListener('click',FoodShow(foodName))
+// searchBtn click than function call
+foodSearch.addEventListener('click',SearchMeals)
