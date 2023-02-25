e441424da3bee9e9870b5a48be2e30742e039772
atiqur <atiqurrahman.cse2552@gmail.com>
2023-02-25 18:01:15 +0600

    function update

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
 const foodSearch=document.getElementById('foodSearchBtn')
+let moreThan6=0
 
-
-const LoadFood=(searchText)=>{
+const LoadFood=(searchText='fish')=>{
     foodUrl=`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}
     `
     fetch(foodUrl)
     .then(res=>res.json())
-    .then(data=>foodDisplay(data.meals))
+    .then(data=>{
+      foodDisplay(data.meals)
+    })
+    
     // if error 
     .catch(error=>console.log(error))
+}
+
+function setElement(meal){
+  const cardContainer=document.getElementById('card-container')
+  const div=document.createElement('div');
+  div.classList.add('col');
+  div.innerHTML=`<div class="card mb-3" style="max-width: 540px">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${meal.strMealThumb}" class="img-fluid rounded-start" alt="..." />
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">
+          ${meal.strInstructions.slice(0,100)}...
+        </p>
+        <a onclick="FoodDetails(${meal.idMeal})" class="text-warning text-decoration-underline " data-bs-toggle="modal" data-bs-target="#FoodDatails">View Details</a>
+      </div>
+    </div>
+  </div>
+  </div>
+  `
+  cardContainer.appendChild(div)
+
 }
 
 const foodDisplay=meals=>{
@@ -17,42 +45,32 @@ const foodDisplay=meals=>{
     // remove item and new item show 
     cardContainer.innerText=''
   
-
     const countItem=meals.length
-   
+    // card  item add 
+
+    const show=document.getElementById('showBtn')
+     show.addEventListener('click',function(){
+      meals.forEach(meal=>{
+      setElement(meal)
+      })
+      showBtn.innerHTML=''
+     })
+
+
     meals.splice(0,6).forEach(meal=>{
-        // console.log(meal)
-        const div=document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML=`<div class="card mb-3" style="max-width: 540px">
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img src="${meal.strMealThumb}" class="img-fluid rounded-start" alt="..." />
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">${meal.strMeal}</h5>
-              <p class="card-text">
-                ${meal.strInstructions.slice(0,100)}...
-              </p>
-             
-              <a onclick="FoodDetails(${meal.idMeal})" class="text-warning text-decoration-underline " data-bs-toggle="modal" data-bs-target="#FoodDatails">View Details</a>
-             
-            </div>
-          </div>
-        </div>
-        </div>
-        `
-        cardContainer.appendChild(div)
+        setElement(meal)
+
     })
+
+   
    // show all btn show when product item e greater than 6 
+   const showBtn=document.getElementById('showBtn')
     if(countItem >=6){
-      const showBtn=document.getElementById('showBtn')
       // <button class="btn btn-warning py-1 px-3">show All</button>
       let div=document.createElement('div')
       
       div.innerHTML=`
-      <button onclick="showAllFood()" class="btn btn-warning py-1 px-3">show All</button>
+      <button id="showBtn" class="btn btn-warning py-1 px-3">show All</button>
       `
       // classList.add('btn','btn-warning','py-1','px-3')
       // btn.innerText='Show All'
@@ -61,14 +79,16 @@ const foodDisplay=meals=>{
       showBtn.appendChild(div)
     }
     else{
-      showBtn.innerText=''
+      console.log(showBtn)
+      showBtn.innerHTML=''
     }
+
+  
 }
 
-const showAllFood=()=>{
-  console.log("show all data ")
-  // foodDisplay(data)
-}
+
+
+
 
 const FoodDetails=(itemId)=>{
   url=`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${itemId}`
@@ -122,8 +142,10 @@ const SearchMeals=()=>{
   }
  
 }
+
+
 // when page load than call 
-LoadFood('fish')
+LoadFood()
 
 // window.addEventListener('load',FoodShow('fish'))
 // searchBtn click than function call
